cde9435ba5aef28aa60da1a2083c578b854f538d
Trayek <jzi@sitecore.net>
2023-05-23 21:28:30 -0400

    Updated css stuff to fix Vercel build

--- src/rendering/src/components/Humber/NewsletterForm.tsx
+++ src/rendering/src/components/Humber/NewsletterForm.tsx
@@ -2,7 +2,7 @@
 const NewsletterForm = (): JSX.Element => {
     return (
     <>
-        <div data-mooform-id="e544ff5a-0485-4837-9b48-7d12338523fe" style={{margin-left: 'auto', margin-right: 'auto'}} ></div>
+        <div data-mooform-id="e544ff5a-0485-4837-9b48-7d12338523fe" cssClass="marginAuto" ></div>
 
         <script
             dangerouslySetInnerHTML={{ __html:
